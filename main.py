a65d88abfb3d810c80a93665046f6efa0b5acab3
George <huntermlg123465@gmail.com>
2024-03-04 06:32:57 +0300

    Cleanups.

--- main.py
+++ main.py
@@ -48,93 +48,6 @@ def get_loader(is_train: bool, batch_size: int, shuffle: bool, transform: transf
     return dataset, loader
 
 
-class AdaptiveDropout(torch.nn.Module):
-    current_epoch: int = 0
-
-    def __init__(self, total_epochs, initial_drop_prob=1e-6, final_drop_prob=0.8, adaptivity_control_start=0.5,
-                 adaptivity_control_end=0.1):
-        super(AdaptiveDropout, self).__init__()
-        self.initial_drop_prob = initial_drop_prob
-        self.final_drop_prob = final_drop_prob
-        self.adaptivity_control_start = adaptivity_control_start
-        self.adaptivity_control_end = adaptivity_control_end
-        self.total_epochs = total_epochs
-        self.drop_prob = initial_drop_prob
-        self.adaptivity_control = adaptivity_control_start
-
-    def forward(self, x):
-        self.update_parameters(self.current_epoch)
-
-        if self.training:
-            active_prob = x.abs().mean(dim=tuple(range(1, x.dim())), keepdim=True)
-            dropout_prob = torch.clamp(self.drop_prob + self.adaptivity_control * (active_prob - self.drop_prob), 0, 1)
-            mask = torch.bernoulli(1.0 - dropout_prob).to(x.device)
-            x = x * mask / (1.0 - dropout_prob)
-        return x
-
-    def update_parameters(self, current_epoch):
-        epoch_ratio = current_epoch / self.total_epochs
-        self.drop_prob = (self.final_drop_prob - self.initial_drop_prob) * epoch_ratio + self.initial_drop_prob
-        self.adaptivity_control = ((self.adaptivity_control_end - self.adaptivity_control_start) * epoch_ratio
-                                   + self.adaptivity_control_start)
-
-    def step(self):
-        self.current_epoch += 1
-
-
-class AdaptiveDropout2D(torch.nn.Module):
-    def __init__(self, total_epochs, initial_drop_prob=1e-6, final_drop_prob=0.8, adaptivity_control_start=0.5,
-                 adaptivity_control_end=0.1):
-        super(AdaptiveDropout2D, self).__init__()
-        self.initial_drop_prob = max(0.0, min(initial_drop_prob, 1.0))
-        self.final_drop_prob = max(0.0, min(final_drop_prob, 1.0))
-        self.adaptivity_control_start = adaptivity_control_start
-        self.adaptivity_control_end = adaptivity_control_end
-        self.total_epochs = total_epochs
-        self.drop_prob = self.initial_drop_prob
-        self.adaptivity_control = adaptivity_control_start
-        self.current_epoch = 0
-
-    def forward(self, x):
-        self.update_parameters(self.current_epoch)
-
-        if self.training:
-            active_prob = x.abs().mean(dim=(2, 3), keepdim=True) if x.dim() > 3 else x.abs().mean(
-                dim=tuple(range(1, x.dim())), keepdim=True)
-
-            dropout_prob = torch.clamp(self.drop_prob + self.adaptivity_control * (active_prob - self.drop_prob), 0, 1)
-
-            mask_shape = (x.shape[0], x.shape[1], 1, 1) if x.dim() > 3 else (x.shape[0], 1)
-            mask = torch.bernoulli(1.0 - dropout_prob.expand_as(x)).to(x.device)
-
-            x = x * mask / (1.0 - dropout_prob + 1e-6)
-
-        return x
-
-    def update_parameters(self, current_epoch):
-        epoch_ratio = min(max(current_epoch / self.total_epochs, 0), 1)  # Clamp ratio to [0, 1]
-        self.drop_prob = (self.final_drop_prob - self.initial_drop_prob) * epoch_ratio + self.initial_drop_prob
-        self.adaptivity_control = ((self.adaptivity_control_end - self.adaptivity_control_start) * epoch_ratio
-                                   + self.adaptivity_control_start)
-
-    def step(self):
-        self.current_epoch = min(self.current_epoch + 1, self.total_epochs)
-
-
-class HomoeostaticRegulation(torch.nn.Module):
-    def __init__(self, target_activity=0.1):
-        super(HomoeostaticRegulation, self).__init__()
-        self.target_activity = target_activity
-
-    def forward(self, x):
-        if self.training:
-            current_activity = x.abs().mean()
-            adjustment_factor = self.target_activity / current_activity
-            x = x * adjustment_factor
-
-        return x
-
-
 class AggregationBlock(torch.nn.Module):
     def __init__(self, in_channels):
         super(AggregationBlock, self).__init__()
@@ -158,7 +71,6 @@ class Net(torch.nn.Module):
     def __init__(self, input_size=(3, 32, 32)):
         super().__init__()
 
-        self.avg_pool = torch.nn.AvgPool2d(2, 1)
         self.max_pool = torch.nn.MaxPool2d(2, 2)
         self.relu = torch.nn.ReLU(inplace=True)
 
@@ -177,15 +89,7 @@ class Net(torch.nn.Module):
         self.conv5 = torch.nn.Conv2d(self.conv4.out_channels, 256, kernel_size=2, stride=1, padding=1)
         self.bn5 = torch.nn.BatchNorm2d(self.conv5.out_channels)
 
-        self.dropout1 = torch.nn.Dropout2d(p=0.8)
-        self.dropout2 = torch.nn.Dropout(p=0.2)
-        self.dropout3 = torch.nn.Dropout2d(p=0.1)
-
-        self.adaptive_dropout_2d = AdaptiveDropout(num_epochs,
-                                                   initial_drop_prob=0.2,
-                                                   final_drop_prob=0.8,
-                                                   adaptivity_control_start=0.2,
-                                                   adaptivity_control_end=0.7)
+        self.dropout_2d = torch.nn.Dropout2d(p=0.35)
 
         self.block_1 = AggregationBlock(in_channels=self.conv2.out_channels)
         self.block_2 = AggregationBlock(in_channels=self.conv4.out_channels)
@@ -195,44 +99,43 @@ class Net(torch.nn.Module):
             x = self.forward_conv(dummy_input)
             out_features = x.view(x.size(0), -1).size(1)
 
-        self.fc1 = torch.nn.Linear(out_features, 128)
-        self.bn5_1d = torch.nn.BatchNorm1d(self.fc1.out_features)
-        self.fc2 = torch.nn.Linear(self.fc1.out_features, 10)
+        print(f"Conv out features: {out_features}")
+        self.fc = torch.nn.Linear(out_features, 10)
 
     def forward_conv(self, x):
         x = self.conv1(x)
         x = self.bn1(x)
         x = self.relu(x)
-        x = self.dropout2(x)
+        x = self.dropout_2d(x)
         x = self.max_pool(x)
 
         x1 = self.conv2(x)
         x1 = self.bn2(x1)
         x1 = self.relu(x1)
-        x1 = self.dropout3(x1)
+        x1 = self.dropout_2d(x1)
+        x1 = self.max_pool(x1)
 
         x2 = self.conv3(x1)
         x2 = self.bn3(x2)
         x2 = self.relu(x2)
-        x2 = self.dropout3(x2)
+        x2 = self.dropout_2d(x2)
+        x2 = self.max_pool(x2)
 
         x = self.block_1.forward(x1, x2)
-        x = self.avg_pool(x)
-        # x = self.adaptive_dropout_2d(x)
 
         x3 = self.conv4(x)
         x3 = self.bn4(x3)
         x3 = self.relu(x3)
-        x3 = self.dropout3(x3)
+        x3 = self.dropout_2d(x3)
+        x3 = self.max_pool(x3)
 
         x4 = self.conv5(x3)
         x4 = self.bn5(x4)
         x4 = self.relu(x4)
-        x4 = self.dropout3(x4)
+        x4 = self.dropout_2d(x4)
+        x4 = self.max_pool(x4)
 
         x = self.block_2.forward(x3, x4)
-        x = self.avg_pool(x)
-        # x = self.dropout1(x)
 
         return x
 
@@ -241,18 +144,9 @@ class Net(torch.nn.Module):
 
         x = torch.flatten(x, 1)
 
-        x = self.fc1(x)
-        x = self.bn5_1d(x)
-        x = self.relu(x)
-        # x = self.dropout2(x)
-
-        x = self.fc2(x)
+        x = self.fc(x)
 
         return x
-
-    def step(self):
-        self.adaptive_dropout_2d.step()
-        # self.adaptive_dropout2.step()
 
 
 train_batch_size = 128
@@ -310,7 +204,7 @@ print(' '.join(f'{classes[labels[j]]:5s}' for j in range(test_batch_size)))
 
 lr = 1e-3
 weight_decay = 1e-1
-num_epochs = 100
+num_epochs = 300
 
 net = Net()
 net.to(device)
@@ -318,7 +212,7 @@ net.to(device)
 criterion = torch.nn.CrossEntropyLoss()
 optimizer = optim.AdamW(net.parameters(), lr=lr, weight_decay=weight_decay)
 
-scheduler = torch.optim.lr_scheduler.ReduceLROnPlateau(optimizer, mode='min', factor=0.4, patience=2, verbose=True)
+scheduler = torch.optim.lr_scheduler.OneCycleLR(optimizer, max_lr=lr, steps_per_epoch=len(train_loader), epochs=num_epochs)
 
 print('LEARNING')
 writer = SummaryWriter()
@@ -338,6 +232,7 @@ for epoch in range(num_epochs):
         loss = criterion(outputs, labels)
         loss.backward()
         optimizer.step()
+        scheduler.step()
 
         running_loss += loss.item()
         _, predicted = torch.max(outputs.data, 1)
@@ -374,8 +269,6 @@ for epoch in range(num_epochs):
           f'Val Loss: {val_loss / len(val_loader):.4f}, '
           f'Val Accuracy: {val_accuracy:.2f}%')
 
-    scheduler.step(val_loss)
-    net.step()
 
 writer.flush()
 
